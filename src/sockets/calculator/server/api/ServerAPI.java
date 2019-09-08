54796d797d2bece6af62b03f5bd259038f724ba7
jamez <jamersonalsilva14@gmail.com>
2019-09-08 16:01:37 -0300

    feature: ServerConnectionSingle

--- src/sockets/calculator/server/api/ServerAPI.java
+++ src/sockets/calculator/server/api/ServerAPI.java
@@ -1,9 +1,9 @@
 package sockets.calculator.server.api;
 
-import sockets.calculator.server.controller.ServerControllerMulti;
+import sockets.calculator.server.controller.ServerControllerSingle;
 
 public class ServerAPI {
     public static void main(String[] args) {
-        ServerControllerMulti serverC = new ServerControllerMulti("127.0.0.1", 3333);
+        ServerControllerSingle serverC = new ServerControllerSingle("127.0.0.1", 3333);
     }
 }
